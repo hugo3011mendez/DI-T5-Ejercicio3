08a83ff5b942b4f4230607769e4962f7388f543b
Hugo Mendez <hugo3011mendez@gmail.com>
2021-02-10 20:45:03 +0100

    - Eliminado Form1 y añadido formulario Ejercicio3 debido a un error en el Designer de Form1.

--- Ejercicio3/Ejercicio3.cs
+++ Ejercicio3/Ejercicio3.cs
@@ -10,9 +10,9 @@ using System.Windows.Forms;
 
 namespace Ejercicio3
 {
-    public partial class Form1 : Form
+    public partial class Ejercicio3 : Form
     {
-        public Form1()
+        public Ejercicio3()
         {
             InitializeComponent();
         }
